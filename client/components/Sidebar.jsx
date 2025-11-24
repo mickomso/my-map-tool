86c5204ded0dd23d33e260144a6e7d396d0e24ed
Miguel Company <l>
2025-11-24 14:14:38 +0100

    feat: Add sublayers with visibility icon

--- client/components/Sidebar.jsx
+++ client/components/Sidebar.jsx
@@ -1,18 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Paper,
   Typography,
   IconButton,
   List,
-  ListItem,
+  ListItemButton,
   ListItemText,
   Divider,
+  Collapse,
 } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import MenuIcon from '@mui/icons-material/Menu';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 const Sidebar = () => {
-  const [open, setOpen] = React.useState(true);
+  const [open, setOpen] = useState(true);
+  const [openLayers, setOpenLayers] = useState({});
+  const [visibleLayers, setVisibleLayers] = useState({
+    'Layer 1': true,
+    'Layer 2': false,
+    Traffic: true,
+    'Sublayer 1.1': true,
+    'Sublayer 1.2': true,
+    'Sublayer 2.1': true,
+    'Sublayer 2.2': true,
+    'Sublayer 2.3': true,
+    Congestion: true,
+    Incidents: true,
+  });
+
+  const handleToggleLayer = (layerName) => {
+    setOpenLayers((prev) => ({ ...prev, [layerName]: !prev[layerName] }));
+  };
+
+  const handleToggleVisibility = (layerName, e) => {
+    e.stopPropagation();
+    setVisibleLayers((prev) => ({ ...prev, [layerName]: !prev[layerName] }));
+  };
+
+  const layers = [
+    {
+      name: 'Layer 1',
+      status: 'Visible',
+      sublayers: ['Sublayer 1.1', 'Sublayer 1.2'],
+    },
+    {
+      name: 'Layer 2',
+      status: 'Hidden',
+      sublayers: ['Sublayer 2.1', 'Sublayer 2.2', 'Sublayer 2.3'],
+    },
+    {
+      name: 'Traffic',
+      status: 'Enabled',
+      sublayers: ['Congestion', 'Incidents'],
+    },
+  ];
 
   if (!open) {
     return (
@@ -65,15 +110,40 @@ const Sidebar = () => {
       </div>
       <Divider />
       <List sx={{ overflowY: 'auto' }}>
-        <ListItem button>
-          <ListItemText primary='Layer 1' secondary='Visible' />
-        </ListItem>
-        <ListItem button>
-          <ListItemText primary='Layer 2' secondary='Hidden' />
-        </ListItem>
-        <ListItem button>
-          <ListItemText primary='Traffic' secondary='Enabled' />
-        </ListItem>
+        {layers.map((layer) => (
+          <React.Fragment key={layer.name}>
+            <ListItemButton onClick={() => handleToggleLayer(layer.name)}>
+              <IconButton
+                size='small'
+                onClick={(e) => handleToggleVisibility(layer.name, e)}
+                sx={{ mr: 1 }}
+              >
+                {visibleLayers[layer.name] ? <Visibility /> : <VisibilityOff />}
+              </IconButton>
+              <ListItemText
+                primary={layer.name}
+                secondary={visibleLayers[layer.name] ? 'Visible' : 'Hidden'}
+              />
+              {openLayers[layer.name] ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+            <Collapse in={openLayers[layer.name]} timeout='auto' unmountOnExit>
+              <List component='div' disablePadding>
+                {layer.sublayers.map((sublayer) => (
+                  <ListItemButton key={sublayer} sx={{ pl: 4 }}>
+                    <IconButton
+                      size='small'
+                      onClick={(e) => handleToggleVisibility(sublayer, e)}
+                      sx={{ mr: 1 }}
+                    >
+                      {visibleLayers[sublayer] ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                    <ListItemText primary={sublayer} />
+                  </ListItemButton>
+                ))}
+              </List>
+            </Collapse>
+          </React.Fragment>
+        ))}
       </List>
     </Paper>
   );
