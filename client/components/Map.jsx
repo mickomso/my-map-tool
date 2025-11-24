1ffbfb20674fa15b8d587827f59fb6e090423936
Miguel Company <l>
2025-11-24 11:28:58 +0100

    fix: add mapbox new style background map

---
+++ client/components/Map.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import Map, { NavigationControl } from 'react-map-gl';
+import { Meteor } from 'meteor/meteor';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const MapComponent = () => {
+  const mapboxSettings = Meteor.settings.public?.mapbox || {};
+
+  const [viewState, setViewState] = useState({
+    longitude: mapboxSettings.defaultCenter?.lng || -3.7038,
+    latitude: mapboxSettings.defaultCenter?.lat || 40.4168,
+    zoom: mapboxSettings.defaultZoom || 12,
+  });
+
+  return (
+    <div style={{ width: '100%', height: '100vh' }}>
+      <Map
+        {...viewState}
+        onMove={(evt) => setViewState(evt.viewState)}
+        style={{ width: '100%', height: '100%' }}
+        mapStyle={mapboxSettings.styleURL || 'mapbox://styles/mapbox/streets-v12'}
+        mapboxAccessToken={mapboxSettings.accessToken}
+      >
+        <NavigationControl position='top-right' />
+      </Map>
+    </div>
+  );
+};
+
+export default MapComponent;
